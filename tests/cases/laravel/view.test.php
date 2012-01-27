133a98a676fe88f23e2b3ebda9c4f47c9b8106e8
Phill Sparks <me@phills.me.uk>
2012-01-27 14:48:27 +0000

    Merge tag 'v3.0.0-beta-2' of laravel

--- tests/cases/laravel/view.test.php
+++ tests/cases/laravel/view.test.php
@@ -3,6 +3,25 @@
 class ViewTest extends PHPUnit_Framework_TestCase {
 
 	/**
+	 * Tear down the testing environment.
+	 */
+	public function tearDown()
+	{
+		View::$shared = array();
+		Event::$events = array();
+	}
+
+	/**
+	 * Test the View::make method.
+	 *
+	 * @group laravel
+	 */
+	public function testMakeMethodReturnsAViewInstance()
+	{
+		$this->assertInstanceOf('Laravel\\View', View::make('home.index'));
+	}
+
+	/**
 	 * Test the View class constructor.
 	 *
 	 * @group laravel
@@ -36,6 +55,42 @@ class ViewTest extends PHPUnit_Framework_TestCase {
 		$view = new View('home.index', array('name' => 'Taylor'));
 
 		$this->assertEquals('Taylor', $view->data['name']);
+	}
+
+	/**
+	 * Test the View::name method.
+	 *
+	 * @group laravel
+	 */
+	public function testNameMethodRegistersAViewName()
+	{
+		View::name('home.index', 'home');
+
+		$this->assertEquals('home.index', View::$names['home']);
+	}
+
+	/**
+	 * Test the View::shared method.
+	 *
+	 * @group laravel
+	 */
+	public function testSharedMethodAddsDataToSharedArray()
+	{
+		View::share('comment', 'Taylor');
+
+		$this->assertEquals('Taylor', View::$shared['comment']);
+	}
+
+	/**
+	 * Test the View::with method.
+	 *
+	 * @group laravel
+	 */
+	public function testViewDataCanBeSetUsingWithMethod()
+	{
+		$view = View::make('home.index')->with('comment', 'Taylor');
+
+		$this->assertEquals('Taylor', $view->data['comment']);
 	}
 
 	/**
@@ -104,6 +159,79 @@ class ViewTest extends PHPUnit_Framework_TestCase {
 		$view['comment'] = 'Taylor';
 
 		$this->assertEquals('Taylor', $view['comment']);
+	}
+
+	/**
+	 * Test the View::nest method.
+	 *
+	 * @group laravel
+	 */
+	public function testNestMethodSetsViewInstanceInData()
+	{
+		$view = View::make('home.index')->nest('partial', 'tests.basic');
+
+		$this->assertEquals('tests.basic', $view->data['partial']->view);
+
+		$this->assertInstanceOf('Laravel\\View', $view->data['partial']);
+	}
+
+	/**
+	 * Test that the registered data is passed to the view correctly.
+	 *
+	 * @group laravel
+	 */
+	public function testDataIsPassedToViewCorrectly()
+	{
+		View::share('name', 'Taylor');
+
+		$view = View::make('tests.basic')->with('age', 25)->render();
+
+		$this->assertEquals('Taylor is 25', $view);
+	}
+
+	/**
+	 * Test that the View class renders nested views.
+	 *
+	 * @group laravel
+	 */
+	public function testNestedViewsAreRendered()
+	{
+		$view = View::make('tests.basic')
+								->with('age', 25)
+								->nest('name', 'tests.nested');
+
+		$this->assertEquals('Taylor is 25', $view->render());
+	}
+
+	/**
+	 * Test that the View class renders nested responses.
+	 *
+	 * @group laravel
+	 */
+	public function testNestedResponsesAreRendered()
+	{
+		$view = View::make('tests.basic')
+								->with('age', 25)
+								->with('name', Response::view('tests.nested'));
+
+		$this->assertEquals('Taylor is 25', $view->render());
+	}
+
+	/**
+	 * Test the View class raises a composer event.
+	 *
+	 * @group laravel
+	 */
+	public function testComposerEventIsCalledWhenViewIsRendering()
+	{
+		View::composer('tests.basic', function($view)
+		{
+			$view->data = array('name' => 'Taylor', 'age' => 25);
+		});
+
+		$view = View::make('tests.basic')->render();
+
+		$this->assertEquals('Taylor is 25', $view);
 	}
 
 }
