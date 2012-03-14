819bae9cde4edd20d001e5bd4c64c4512bf65db1
Phill Sparks <me@phills.me.uk>
2012-03-14 15:34:44 +0000

    Fix snip language validation.
    Signed-off-by: Phill Sparks <me@phills.me.uk>

--- bundles/snip/model.php
+++ bundles/snip/model.php
@@ -65,7 +65,7 @@ class Model extends Eloquent {
 		$rules = array(
 			'title'    => 'required',
 			'body'     => 'required',
-			'language' => 'required|in:'.implode(',', self::language_options()),
+			'language' => 'required|in:'.implode(',', array_keys(self::language_options())),
 		);
 
 		$validator = new Validator( Input::all(), $rules );
